4dffff86c6134e97664b15a0f07d0012e86844ce
rohit2212kr <rohit2212kr@users.noreply.github.com>
2026-07-10 18:57:20 +0530

    Prepare for Render deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
